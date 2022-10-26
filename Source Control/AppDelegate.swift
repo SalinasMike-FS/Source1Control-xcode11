2df29c5b586078b2d79ac4e374e3c537b3a75460
Mike Salinas <nmsalinas@student.fullsail.edu>
2022-10-26 12:29:00 -0400

    Initial Commit

---
+++ Source Control/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Source Control
+//
+//  Created by Natividad Michael Salinas II on 10/26/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Source Control/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Source Control
+//
+//  Created by Natividad Michael Salinas II on 10/26/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
